5420cd949821c3e3200b44e082ef0cad68846b21
Ev357 <evest.dev@gmail.com>
2024-07-12 20:56:32 +0200

    build: installed supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,22 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
+    },
+  },
+  modules: ["@nuxtjs/supabase"],
+  supabase: {
+    url: process.env.NUXT_PUBLIC_SUPABASE_URL,
+    key: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      exclude: ["/"],
+    },
+  },
   future: {
     compatibilityVersion: 4,
   },
