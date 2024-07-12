00bfb5dac515d0a6c7389b8c958ec5d71ddece32
Ev357 <evest.dev@gmail.com>
2024-07-12 22:47:59 +0200

    build: i18n, seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,8 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "shadcn-nuxt",
     "@nuxt/eslint",
+    "@nuxtjs/seo",
+    "@nuxtjs/i18n",
   ],
   supabase: {
     url: process.env.NUXT_PUBLIC_SUPABASE_URL,
@@ -26,6 +28,27 @@ export default defineNuxtConfig({
   shadcn: {
     prefix: "U",
     componentDir: "./app/components/ui",
+  },
+  site: {
+    name: 'C00kb00k',
+    description: 'C00kb00k',
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+        file: "en.json",
+      },
+      {
+        code: "cs",
+        name: "Čeština",
+        file: "cs.json",
+      },
+    ],
+    lazy: true,
+    langDir: "lang",
   },
   future: {
     compatibilityVersion: 4,
