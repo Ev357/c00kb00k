fb575b7926fc713b64b0f3a93588b09c1c6b46d8
Ev357 <evest.dev@gmail.com>
2024-07-20 11:30:21 +0200

    feat: empty dialog

---
+++ app/components/C00kb00ks/AddDialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <UDialog>
+    <UDialogTrigger as-child>
+      <slot />
+    </UDialogTrigger>
+    <UDialogContent class="sm:max-w-[425px]">
+      <UDialogHeader>
+        <UDialogTitle>Edit profile</UDialogTitle>
+        <UDialogDescription>
+          Make changes to your profile here. Click save when you're done.
+        </UDialogDescription>
+      </UDialogHeader>
+      <div class="grid gap-4 py-4">
+        <div class="grid grid-cols-4 items-center gap-4">
+          <ULabel for="name" class="text-right">Name</ULabel>
+          <UInput id="name" value="Pedro Duarte" class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <ULabel for="username" class="text-right">Username</ULabel>
+          <UInput id="username" value="@peduarte" class="col-span-3" />
+        </div>
+      </div>
+      <UDialogFooter>
+        <UButton type="submit">Save changes</UButton>
+      </UDialogFooter>
+    </UDialogContent>
+  </UDialog>
+</template>
