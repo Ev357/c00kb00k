a0f3909baa3a2afa4ecaaba665a6d0275409b8ef
Ev357 <evest.dev@gmail.com>
2024-11-01 23:29:42 +0100

    feat: nunito font

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,26 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "C00kb00k",
-  description: "C00kb00k",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+	title: 'C00kb00k',
+	description: 'C00kb00k',
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Examples', link: '/markdown-examples' }
+		],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+		sidebar: [
+			{
+				text: 'Examples',
+				items: [
+					{ text: 'Markdown Examples', link: '/markdown-examples' },
+					{ text: 'Runtime API Examples', link: '/api-examples' }
+				]
+			}
+		],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+		socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+	}
+});
