65337d690b60825ad74516c959335866d7b0b185
Ev357 <evest.dev@gmail.com>
2024-11-20 14:27:06 +0100

    feat: title

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages';
+
+	let { name, disableTemplate = false }: { name: string; disableTemplate?: boolean } = $props();
+
+	let title = $derived.by(() => {
+		if (!disableTemplate) {
+			return `${name} - ${m.c00kb00k()}`;
+		}
+
+		return m.c00kb00k();
+	});
+</script>
+
+<title>{title}</title>
---
+++ src/routes/(authed)/settings/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages';
+	import Title from '$lib/components/Title.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<Title name={m.settings()} />
+</svelte:head>
+
+{@render children()}
